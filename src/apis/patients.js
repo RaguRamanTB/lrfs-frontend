a391041f9bbf58429fe4047f3b73b420d16a38a7
Raguraman T B <aakash1956ragu@gmail.com>
2023-12-31 17:22:44 +0530

    LRFS : Updating backend endpoint (GCP App Engine)

--- src/apis/patients.js
+++ src/apis/patients.js
@@ -4,7 +4,7 @@ import axios from "axios";
  * Axios create with base URL and header configuration
  */
 export default axios.create({
-  baseURL: "http://labreportfollowupsystem-env.eba-rys6r3jq.ap-south-1.elasticbeanstalk.com/api",
+  baseURL: "https://lab-report-follow-up-system.el.r.appspot.com/api",
   headers: {
     "Content-type": "application/json",
   },
